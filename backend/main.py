6e61e6ed7169b1ae9334ef60fde3667370aee8db
Brunaveloso7 <brunavelosoribeiro7@gmail.com>
2026-06-21 16:56:46 +0000

    Cria estrutura inicial da API e rota home

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+
 app = FastAPI()
 
 from backend.database import engine
 from backend import models
 
 models.Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def home():
+    return {"mensagem": "API da biblioteca funcionando"}
