b5ce47ba113d922f0f601bb4a1a6408de4dd491a
Brunaveloso7 <brunavelosoribeiro7@gmail.com>
2026-06-21 16:30:14 +0000

    Criando do model Livro para tabela livros

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,11 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 
-
 class Livro(Base):
     __tablename__ = "livros"
 
-
     id = Column(Integer, primary_key=True, index=True)
-    titulo = Column(String, nullable=False)
-    autor = Column(String, nullable=False)
-    url_imagem = Column(String, nullable=False)
+    titulo = Column(String, index=True)
+    autor = Column(String)
+    url_imagem = Column(String)
     disponivel = Column(Boolean, default=True)
